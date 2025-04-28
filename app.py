fad2dd521f1c9a278d3f599a8e696f76fa841598
RAM DHIGHASH NG <100917760+ramsuregokul@users.noreply.github.com>
2025-04-28 09:31:39 +0530

    app.py

--- app.py
+++ app.py
@@ -56,7 +56,8 @@ def update(id):
     else:
         return render_template('update.html', task=task)
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
